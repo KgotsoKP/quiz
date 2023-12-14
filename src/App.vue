0f0a3701b0aef0d840fcdecdfaa072bb2f72e847
Kgotso Phiri <kgotsokg12@gmail.com>
2023-12-14 21:38:09 +0200

    Add and register  questions and result componets

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-
+  <div class="ctr">
+    <app-questions></app-questions>
+    <app-result></app-result>
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
 </template>
 
 <script>
+import AppQuestions from "./components/Questions.vue"
+import AppResult from "./components/Result.vue"
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppResult,
+    AppQuestions,
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>
